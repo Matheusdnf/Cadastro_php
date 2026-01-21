01fcdb45286a88a293dcd886caa2014f0d4b5561
Matheusdnf <matheusdiniz870@gmail.com>
2026-01-20 22:32:20 -0300

    feat: carregamento do env

---
+++ config/env.php
@@ -0,0 +1,19 @@
+<?php
+
+$envPath = dirname(__DIR__) . '/.env';
+
+if (!file_exists($envPath)) {
+    die('.env não encontrado!');
+}
+
+$lines = file($envPath, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+
+foreach ($lines as $line) {
+    if (str_starts_with(trim($line), '#')) {
+        continue;
+    }
+
+    [$key, $value] = explode('=', $line, 2);
+
+    $_ENV[trim($key)] = trim($value);
+}
