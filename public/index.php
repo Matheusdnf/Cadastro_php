d9a2bca7fffec05ae7ea7e10234257fb2fa2bb9a
Matheusdnf <matheusdiniz870@gmail.com>
2026-01-21 16:29:19 -0300

    refactor- organização das rotas e da tela inicial

--- public/index.php
+++ public/index.php
@@ -1,94 +1,71 @@
- <?php
-require __DIR__ . '/../config/config.php';
-require __DIR__ . '/../helpers.php';
-require __DIR__ . '/../app/models/UserController.php';
-require __DIR__ . '/../app/views/layout.php';
+<?php
+require_once __DIR__ . '/../config/config.php';
+require_once __DIR__ . '/../helpers.php';
+require_once __DIR__ . '/../app/models/UserController.php';
+require_once __DIR__ . '/../app/views/layout.php';
 
-renderHeader("Lista de Usuários");
+$p = $_GET['p'] ?? 'lista';          
+$action = $_GET['action'] ?? null;   
 
-$users = UserController::all();
+if ($action === 'edit') {
+    
+    require_once __DIR__ . '/../app/controllers/handle_user.php';
+    exit; 
+}
 
-?>
-<h1>Usuários</h1>
+if ($action === 'delete') {
+    require_once __DIR__ . '/../app/controllers/delete.php';
+    exit;
+}
 
-<?php if ($error = getError()): ?>
+if ($p === 'create') {
 
-  <div style="color:red; margin-bottom:10px"><?= htmlspecialchars($error) ?></div>
+    renderHeader("Novo Usuário");
+    require_once __DIR__ . '/../app/views/create.php';
+    renderFooter();
+} else {
 
-<?php endif; ?>
+    renderHeader("Lista de Usuários");
+    
+    $users = UserController::all();
+    ?>
 
+    <h1>Usuários</h1>
 
-<table>
+    <?php if ($error = getError()): ?>
+        <div style="color:red; margin-bottom:10px"><?= htmlspecialchars($error) ?></div>
+    <?php endif; ?>
 
-<tr>
+    <table>
+        <tr>
+            <th>ID</th>
+            <th>Nome</th>
+            <th>Email</th>
+            <th>Telefone</th>
+            <th>Ações</th>
+        </tr>
+        <?php foreach ($users as $u): ?>
+        <tr>
+            <td><?= $u['id'] ?></td>
+            <td><?= htmlspecialchars($u['nome']) ?></td>
+            <td><?= htmlspecialchars($u['email']) ?></td>
+            <td><?= htmlspecialchars($u['telefone']) ?></td>
+            <td>
+                <button class="btn-edit" data-id="<?= $u['id'] ?>" data-nome="<?= htmlspecialchars($u['nome']) ?>" 
+                        data-email="<?= htmlspecialchars($u['email']) ?>" data-telefone="<?= htmlspecialchars($u['telefone']) ?>">
+                    Editar
+                </button>
+                <button class="btn-delete" data-id="<?= $u['id'] ?>" data-nome="<?= htmlspecialchars($u['nome']) ?>">
+                    Excluir
+                </button>
+            </td>
+        </tr>
+        <?php endforeach; ?>
+    </table>
 
-  <th>ID</th>
+    <script src="<?= BASE_URL ?>js/scripts.js"></script>
 
-  <th>Nome</th>
-
-  <th>Email</th>
-
-  <th>Telefone</th>
-
-  <th>Ações</th>
-
-</tr>
-
-
-<?php foreach ($users as $u): ?>
-
-<tr>
-
-  <td><?= $u['id'] ?></td>
-
-  <td><?= htmlspecialchars($u['nome']) ?></td>
-
-  <td><?= htmlspecialchars($u['email']) ?></td>
-
-  <td><?= htmlspecialchars($u['telefone']) ?></td>
-
-  <td>
-
-    <button
-
-      class="btn-edit"
-
-      data-id="<?= $u['id'] ?>"
-
-      data-nome="<?= htmlspecialchars($u['nome']) ?>"
-
-      data-email="<?= htmlspecialchars($u['email']) ?>"
-
-      data-telefone="<?= htmlspecialchars($u['telefone']) ?>">
-
-      Editar
-
-    </button>
-
-
-    <button
-
-      class="btn-delete"
-
-      data-id="<?= $u['id'] ?>"
-
-      data-nome="<?= htmlspecialchars($u['nome']) ?>">
-
-      Excluir
-
-    </button>
-
-</td>
-
-
-</tr>
-
-<?php endforeach; ?>
-</table>
-
-<script src="<?= BASE_URL ?>js/scripts.js"></script>
-
-<?php include __DIR__ . '/../app/views/modals.php';
-renderFooter();
-
-?> 
+    <?php 
+    include __DIR__ . '/../app/views/modals.php';
+    renderFooter();
+}
